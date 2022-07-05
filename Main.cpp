3b676025ed0ba0f3a35c4f34983fe444d98f11f3
RIVER0906 <103353926+RIVER0906@users.noreply.github.com>
2022-07-06 01:31:43 +0900

    Add files via upload
    ゲームの大まかなシステムは完成
    あとはエフェクト等

--- Main.cpp
+++ Main.cpp
@@ -2,16 +2,17 @@
 
 struct GameData
 {
-	int32 score = 0, highscore = 0;
-	int32 num = 0, ballnum = 10;
+	int32 score = 0, highscore = 0, prescore = 0;
+	int32 num = 0, ballnum = 50;
+	SecondsF timeSet = 20.0s;
 };
 using App = SceneManager<String, GameData>;
 class Title :public App::Scene {
 public:
-	Title(const InitData& init) :IScene(init){
+	Title(const InitData& init) :IScene(init) {
 	}
 	void update() FMT_OVERRIDE {
-		
+
 		if (MouseR.down())
 		{
 			getData().score = 0;
@@ -32,7 +33,7 @@ public:
 		};
 		polygon1.draw(Palette::Skyblue);
 		polygon2.draw(Palette::Skyblue);
-		if (polygon1.mouseOver() )//Right
+		if (polygon1.mouseOver())//Right
 		{
 			polygon1.draw(Palette::White);
 			if (MouseL.down())
@@ -60,40 +61,84 @@ public:
 		}
 		Scene::SetBackground(ColorF(0.3, 0.4, 0.5));
 		FontAsset(U"TitleFont")(U"Color Game").drawAt(400, 100);
-		FontAsset(U"ScoreFont")(U"Score: {}"_fmt(getData().score)).draw(590, 490);
+		FontAsset(U"ScoreFont")(U"Score: {}"_fmt(getData().prescore)).draw(590, 490);
 		FontAsset(U"ScoreFont")(U"HighScore: {}"_fmt(getData().highscore)).draw(520, 540);
 
 	}
 };
 class Game : public App::Scene {
 private:
+	Timer timer{ getData().timeSet, StartImmediately::Yes };
 	ColorF backcolor = Palette::Green;
-	bool gameover = false;
-	Circle player1{ 400, 500, 10 };
+	bool gamefinish = false;
+	bool clear = false;
+	Circle player1{ 300, 500, 10 };
 	Array<Rect>goals;
 	Array<ColorF>goalcolor;
-	int32 n = 0;
+	int32 goalColorNum = 0;
+	Array<int32>goalX, goalY;
+	bool allowance = false;
+	int32 col1 = 0, col2 = 0, col3 = 0;
 	Vec2 RandomGoal()
 	{
-		return RandomVec2({ 50, 750 }, { 50, 450 });
+		return RandomVec2({ 1, 579 }, { 1, 579 });
 	}
-	static constexpr double playerSpeed = 550.0;
+	ColorF setColor(int x) {
+		ColorF color;
+		if (x == 1)color = Palette::Darkcyan;
+		if (x == 2)color = Palette::Darkgreen;
+		if (x == 3)color = Palette::Darkslateblue;
+		return color;
+	}
+	static constexpr double playerSpeed = 300.0;
 public:
 	Game(const InitData& init) : IScene(init) {
 		for (auto i : step(getData().ballnum)) {
-			goals << Rect(RandomGoal().x, RandomGoal().y, 20, 20);
+			goalX << RandomGoal().x;
+			goalY << RandomGoal().y;
+			while (allowance == false) {
+				allowance = true;
+				for (int j = 0; j < i; j++) {
+					if (abs(goalX[i] - goalX[j]) < 21 && abs(goalY[i] - goalY[j]) < 21 || abs(goalX[i] - 290) < 21 && abs(goalY[i] - 490) < 21) {
+						allowance = false;
+						goalX[i] = RandomGoal().x;
+						goalY[i] = RandomGoal().y;
+						break;
+					}
+				}
+			}
+			allowance = false;
+			goals << Rect(goalX[i], goalY[i], 20, 20);
 		}
 		for (const auto& goal : goals)
 		{
-
-			if ((goal.x + goal.y) % 3 == 0) {
-				goalcolor << Palette::Aliceblue;
-			}
-			else if ((goal.x + goal.y) % 3 == 1) {
-				goalcolor << Palette::Antiquewhite;
+			if (Max({ col1,col2,col3 })>col1+col2+col3- Max({ col1,col2,col3 })) {
+				if (Max({ col1,col2,col3 }) == col1) {
+					goalcolor << setColor(2);
+					col2++;
+				}
+				else if (Max({ col1,col2,col3 }) == col2) {
+					goalcolor << setColor(3);
+					col3++;
+				}
+				else{
+					goalcolor << setColor(1);
+					col1++;
+				}
 			}
 			else {
-				goalcolor << Palette::Burlywood;
+				if ((goal.x + goal.y) % 3 == 0) {
+					goalcolor << setColor(1);
+					col1++;
+				}
+				else if ((goal.x + goal.y) % 3 == 1) {
+					goalcolor << setColor(2);
+					col2++;
+				}
+				else {
+					goalcolor << setColor(3);
+					col3++;
+				}
 			}
 		}
 	}
@@ -106,14 +151,25 @@ public:
 				for (int i = 0; i < goals.size(); i++) {
 					if (*it1 == goals[i]) {
 						for (auto it2 = goalcolor.begin(); it2 != goalcolor.end();) {
-							if (n == i) {
-								backcolor = goalcolor[i];
-								it1 = goals.erase(it1);
-								it2 = goalcolor.erase(it2);
-								n = 0;
-								break;
+							if (goalColorNum == i) {
+								if (backcolor == goalcolor[i]) {
+									gamefinish = true;
+									it1 = goals.erase(it1);
+									it2 = goalcolor.erase(it2);
+									goalColorNum = 0;
+									break;
+								}
+								else {
+									timer = Timer{ getData().timeSet, StartImmediately::Yes };
+									backcolor = goalcolor[i];
+									it1 = goals.erase(it1);
+									it2 = goalcolor.erase(it2);
+									goalColorNum = 0;
+									getData().score += 10;
+									break;
+								}
 							}
-							n++;
+							goalColorNum++;
 							++it2;
 						}
 						break;
@@ -124,32 +180,51 @@ public:
 				++it1;
 			}
 		}
-		
+
 		if (goals.size() == 0) {
-			gameover = true;
+			clear = true;
+			gamefinish = true;
 		}
-		if (gameover==true)
+		if (timer.reachedZero()) {
+			gamefinish = true;
+		}
+		if (gamefinish == true)
 		{
-			changeScene(U"Title");
+			getData().prescore= getData().score;
+			getData().highscore = Max(getData().highscore, getData().score);
+			changeScene(U"Title",2.0s);
+			
 		}
 		const double deltaTime = Scene::DeltaTime();
 		const Vec2 move = Vec2(KeyRight.pressed() - KeyLeft.pressed(), KeyDown.pressed() - KeyUp.pressed())
 			.setLength(deltaTime * playerSpeed * (KeyShift.pressed() ? 0.5 : 1.0));
 		player1.moveBy(move);
 		if (KeyZ.down()) {
-			Console << goalcolor;
 			Console << goals;
 		}
-		getData().score += static_cast<int32>(Cursor::Delta().length() * 10);
 	}
 	void draw() const override
 	{
 		Scene::SetBackground(backcolor);
-		for (auto i: step (goals.size())) {
+		RectF{ 600,0, 200, 600 - 500 * timer.progress0_1() }.draw(Palette::White);
+		Line(600, 100, 800, 100).draw(Palette::Red);
+		RectF{ 600,600 - 500 * timer.progress0_1(), 200, 500 * timer.progress0_1() }.draw(Palette::Black);
+		for (auto i : step(goals.size())) {
 			goals[i].draw(goalcolor[i]);
 		}
 		player1.drawFrame(1, 1, Palette::Black);
-		FontAsset(U"ScoreFont")(U"Score: {}"_fmt(getData().score)).draw(40, 40, Palette::Black);
+		FontAsset(U"ScoreFont")(U"Score: {}"_fmt(getData().score)).draw(640, 35, Palette::Black);
+	}
+
+	void drawFadeOut(double t) const override
+	{
+		draw();
+		if (clear == false) {
+			Circle{ player1.x,player1.y, t * 1000 }.draw(Palette::Black);
+		}
+		else {
+			Circle{ player1.x,player1.y, t * 1000 }.draw(Palette::White);
+		}
 	}
 };
 void Main()
@@ -162,7 +237,6 @@ void Main()
 	// タイトルシーン（名前は U"Title"）を登録
 	manager.add<Title>(U"Title");
 	manager.add<Game>(U"Game");
-
 	manager.init(U"Game");
 
 	while (System::Update())
